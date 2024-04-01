4d4dde8cdb3179e409e5d8ff4fedaa12d2bbfdc4
victorferraz364 <victorferraz364@gmail.com>
2024-03-31 22:17:47 -0300

    filter per month

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,28 +4,41 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class Program {
 
-		private static final String RESOURCES = "src\\resources\\";
+	private static final String RESOURCES = "src\\resources\\";
 
-		public static void main(final String... args) throws IOException {
-		
+	public static void main(final String... args) throws IOException {
+
 		final Path path = Paths.get(RESOURCES, "extrato.csv");
-		
-		
+
 		final List<String> lines = Files.readAllLines(path);
+
 		double total = 0d;
+
+		final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
 		for(final String line: lines) {
-		final String[] columns = line.split(",");
-		final double amount = Double.parseDouble(columns[1]);
-		total += amount;
+			
+			String[] columns = line.split(",");
+			LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
+			
+			if(date.getMonth() == Month.JANUARY) {
+
+				double amount = Double.parseDouble(columns[1]);
+				total += amount;
+
+			}
 
 		}
 
 		System.out.println("The total for all transactions is " + total);
 
-		}
-
 	}
+
+}
